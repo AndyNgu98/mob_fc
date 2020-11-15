9882c17982e82d372d9bbdf95feee67fb2bf6af7
AndyNgu98 <andyyn8@gmail.com>
2020-11-15 23:15:49 +0000

    instamedia

--- routes/index.js
+++ routes/index.js
@@ -21,103 +21,23 @@ router.get('/', function(req, res, next) {
       }
   }
 
-  retrieveId()
+ retrieveId()
+
   
   // -------- THIS IS THE WEEKLY REFRESH OF THE INSTAGRAM MEDIA ------------
   
   // RUNNING ALL FUNCTIONS
-  function retrieveId() {
-      pool.getConnection((err, connection ) => {
-          pool.query(`SELECT access_token FROM config WHERE ID=?`, `instagram`, (err, res) => {
-              // REFRESH MEDIA FUNCTION(res[0].access_token)
-              mediaCheck(res[0].access_token)
-              refreshToken(res[0].access_token)
-              
-          })
-      })
-  }
+function retrieveId() {
+  pool.getConnection((err, connection ) => {
+      
+  })
+}  
   
-  // ----------- THIS IS REFRESH TOKEN AND UPDATE INTO DATABASE --------------
-  
-  // REFRESH TOKEN
-  function refreshToken(accessToken) {
-      axios.get(`https://graph.instagram.com/refresh_access_token`, {
-          params: {
-              access_token: accessToken,
-              grant_type: 'ig_refresh_token'
-          }
-      })
-      .then(response => { 
-          // UPDATE TOKEN INTO DB
-          pool.getConnection((err, connection) => {
-              const instaToken = response.data.access_token
-              pool.query(`UPDATE config SET access_token =? WHERE ID =?`, [instaToken,`instagram`], (error, response) => {
-                  console.log(response) 
-              })
-          });
-      })
-      .catch(error => {
-          console.log(error)
-      })
-  } 
-  
-  // ------ THIS IS RETRIEVE MEDIA INFORMATION AND UPDATE TO DATABASE --------------
-  
-  // REFRESH MEDIA FUNCTION
-  
-  function mediaCheck(mediaToken) {
-      axios.get(`${media.url.ROOT}/${media.auth.USER_ID}/media`, {
-          params: {
-              access_token: mediaToken,
-              fields: 'media_type,media_url,permalink,thumbnail_url,timestamp'
-          }
-      })
-      .then(response => {
-        
-          pool.getConnection((err, connection ) => {
-              pool.query(`DELETE FROM instagram;`,(error, res ) =>  {
-                console.log(`delete: ${res}`)
-              })
-              const mediaArray = response.data.data.filter(obj => obj.media_type === 'IMAGE')
-                  console.log(mediaArray)
-                  mediaArray.slice(0, 5).forEach(obj => {
-                      pool.query(`INSERT INTO instagram SET media_type =?, media_url =?,  permalink =?, thumbnail_url =?`, 
-                      [obj.media_type, obj.media_url, obj.permalink, obj.thumbnail_url ], (error, response) => {
-                          console.log(`insert:${response}`)
-                      })
-                  }) 
-          })
-      })
-      .catch(error => {
-          console.log(error)
-      })
-  }
-  
-  
-  // ---------------- GET EXISITNG TOKEN AND REFRESHES IT SAVING INTO DB AND UPDATES MEDIA INTO DB ---------------------- 
-  function getCurrenttoken() {
-      pool.getConnection((err, connection) => {
-          connection.query(`SELECT access_token FROM config WHERE instagramID = 1`, (error, token) => {
-              refreshToken(token)
-          })
-          if(err) {
-              console.log(err);
-          } else {
-              mediaCheck()
-          }
-      });
-  }
-
-
-
-  
-
   //Get Results from DB
   pool.getConnection((err, connection) => {
     connection.query(`SELECT * FROM results`, (error, points) => {
 
       const scores = []
-
 
       points.forEach(point => {
         scores.push({
@@ -144,32 +64,33 @@ router.get('/', function(req, res, next) {
             })
           })
 
-            connection.query(`SELECT * FROM instagram`, (error, insta) => {
-                
-              const instagram = []
+          pool.query(`SELECT access_token FROM config WHERE ID=?`, `instagram`, (err, instares) => {
+            // REFRESH MEDIA FUNCTION(res[0].access_token)
+            axios.get(`${media.url.ROOT}/${media.auth.USER_ID}/media`, {
+              params: {
+                  access_token: instares[0].access_token,
+                  fields: 'media_type,media_url,permalink,thumbnail_url,timestamp'
+              }
+            })
+            .then(response => {
+              const mediaArray = response.data.data.filter(obj => obj.media_type === 'IMAGE') 
+              console.log(mediaArray.slice(0, 5))
+
+              const data = {
+                title: 'MOB-FC',
+                instagram: mediaArray.slice(0, 5),
+                scores,
+                fixtures,
     
-              insta.forEach(insta => {
-                instagram.push({
-                  media: insta.media_url,
-                  link: insta.permalink
-                  
-                })
-              })
-                    const data = {
-                      title: 'MOB-FC',
-                      instagram,
-                      scores,
-                      fixtures,
+              }
+              res.render('index', data);
+            })
+            .catch(error => {
+                console.log(error)
+            })          
+          })
 
-                    }
-
-                    res.render('index', data);
-                  // const savedData = data
-
-                  // res.render('index', savedData)
-
-            });      
-
+          
         });      
     });
   })
@@ -199,16 +120,3 @@ module.exports = router;
 
 
 
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   const data = {
-//     title: 'Home',
-
-//   }
-//   res.render('index', data);
-// });
-
-// module.exports = router;
